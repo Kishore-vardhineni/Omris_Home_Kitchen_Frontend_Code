9ecb0a8b3cddb3f2a504b05306e338f8b04a1d60
kishor <kishore.vardhineni01@gmail.com>
2026-08-01 00:02:05 +0530

    remove the one origin from CORS

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    origin: 'https://omris-home-kitchen-frontend.vercel.app',
+    origin: process.env.CLIENT_URL || 'https://omris-home-kitchen-frontend.vercel.app',
     credentials: true,
   })
 );
