7f982a9426e2e3cfd15b13fd45edf3fd07d8d44c
kishor <kishore.vardhineni01@gmail.com>
2026-08-04 00:06:41 +0530

    server environment config changed

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'https://omris-home-kitchen-frontend.vercel.app',
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
