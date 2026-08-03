a5ad7786e6c97817daa8c0175ba131302a6edbca
kishor <kishore.vardhineni01@gmail.com>
2026-08-04 00:40:39 +0530

    Server file

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://omris-home-kitchen-frontend.vercel.app'],
     credentials: true,
   })
 );
